e6ef8a9b10b69fbeb6f26b1c09ec5573fd7ef8ef
Jason Dye <39197318+jadye527@users.noreply.github.com>
2025-05-14 18:48:48 -0400

    Update run-docker.ps1

--- run-docker.ps1
+++ run-docker.ps1
@@ -3,21 +3,21 @@ param(
     [string]$containerName = "logicapp-docgen-local"
 )
 
-Write-Host "📦 Building Docker image..."
+  Write-Host "📦 Building Docker image..."
 docker build -t logicapp-docgen $projectPath
 
 Write-Host "`n🧹 Stopping previous container (if running)..."
 docker rm -f $containerName 2>$null
 
- <# Write-Host "`n🚀 Running Docker container with interactive shell and logs..."
+  <#    Write-Host "`n🚀 Running Docker container with interactive shell and logs..."
 
  docker run -it `
     -v ${projectPath}:/app `
     --name $containerName `
     logicapp-docgen
 
-Write-Host "`n✅ Container exited. Use logs above to debug if needed."  #> 
-
+Write-Host "`n✅ Container exited. Use logs above to debug if needed." #>  #>  #> #> #> #>
+ #>
 <# docker rm -f $containerName 2>$null
 docker run -it `
 -v ${projectPath}:/app `
@@ -29,7 +29,18 @@ bash
 
  Write-Host "`n✅ Container exited. Use logs above to debug if needed." 
   #>
- docker run -it `
+ <#     docker run -it `
+  -v "${projectPath}:/app" `
+  --name $containerName `
+  logicapp-docgen `
+  python cli_updated.py `
+    --template "600-AD-Powershell-RemoveGroups-template.json" `
+    --parameters "600-AD-Powershell-RemoveGroups-parameters.json" `
+    --docx_template "template.docx" `
+    --output "output/logicapp_test.docx"
+Write-Host "`n✅ Container exited. Use logs above to debug if needed." #> #> #>
+  #> #>
+   docker run -it `
   -v "${projectPath}:/app" `
   --name $containerName `
   logicapp-docgen `
@@ -37,7 +48,6 @@ bash
     --template "450-SendOnboardingEmailtoNewEmployee-template.json" `
     --parameters "450-SendOnboardingEmailtoNewEmployee-parameters.json" `
     --docx_template "template.docx" `
-    --output "output/logicapp_test.docx"
-Write-Host "`n✅ Container exited. Use logs above to debug if needed." #>
- 
-
+   #--output "output/logicapp_test.docx"
+Write-Host "`n✅ Container exited. Use logs above to debug if needed." #> #>
+ #> #>
